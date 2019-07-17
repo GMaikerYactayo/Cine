a449593e1652b2fb5e865ca80a9b3cd011dc8d8c
AHMarcos <marcos.alarcon@vallegrande.edu.pe>
2019-07-17 18:09:06 -0500

    CRUD Cliente no funcional (Eliminar)

---
+++ Cine/src/java/controlador/ClienteC.java
@@ -0,0 +1,146 @@
+package controlador;
+
+import dao.ClienteImpl;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import modelo.Cliente;
+
+/**
+ *
+ * @author Ctorres
+ */
+@Named(value = "ClienteC")
+@SessionScoped
+public class ClienteC implements Serializable {
+
+    Cliente persona = new Cliente();
+    Cliente personaEdit = new Cliente();
+    ClienteImpl dao;
+    List<Cliente> listper;
+    private boolean bt;
+
+    @PostConstruct
+    public void inicio() {
+        try {
+            listar("A");
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void registrar() {
+        dao = new ClienteImpl();
+        try {
+            dao.regitrar(persona);
+            listar("A");
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "REGISTRO COMPLETO", null));
+            limpiar();
+        } catch (Exception e) {
+        }
+    }
+
+    public void modificar() {
+        dao = new ClienteImpl();
+        try {
+            dao.modificar(personaEdit);
+            listar("A");
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "MODIFICACION COMPLETA", null));
+        } catch (Exception e) {
+            System.out.println("Error controlador: " + e);
+        }
+    }
+
+    public void listar(String estado) {
+        dao = new ClienteImpl();
+        try {
+            listper = dao.listarcli(estado);
+        } catch (Exception e) {
+            System.out.println("Erroe: " + e);
+        }
+    }
+
+    public void habilitar(Cliente cli)throws Exception{
+        dao = new ClienteImpl();
+        try {
+            dao.eliminar(cli);
+            listar("I");
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "HABILITACION COMPLETA", null));
+        } catch (Exception e) {
+        }
+    }
+    
+    public void invilitar(Cliente cli) throws Exception{
+         dao = new ClienteImpl();
+        try {
+            dao.eliminar(cli);
+            listar("A");
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO, "INAVILITACION COMPLETA", null));
+        } catch (Exception e) {
+        }
+    }
+//      public void REPORTECLIENTE(String idcli) throws Exception {
+//        report reportCLIENTE = new report();
+//        try {
+//            Map<String, Object> parameters = new HashMap(); // Libro de parametros
+//            parameters.put(null, idcli); //Insertamos un parametro
+//            reportCLIENTE.exportarCLIENTE(parameters); //Pido exportar Reporte con los parametros
+////            report.exportarPDF2(parameters);
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//    }
+    
+    public void limpiar() {
+        persona = new Cliente();
+    }
+
+    public Cliente getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Cliente persona) {
+        this.persona = persona;
+    }
+
+    public ClienteImpl getDao() {
+        return dao;
+    }
+
+    public void setDao(ClienteImpl dao) {
+        this.dao = dao;
+    }
+
+    public List<Cliente> getListper() {
+        return listper;
+    }
+
+    public void setListper(List<Cliente> listper) {
+        this.listper = listper;
+    }
+
+    public Cliente getPersonaEdit() {
+        return personaEdit;
+    }
+
+    public void setPersonaEdit(Cliente personaEdit) {
+        this.personaEdit = personaEdit;
+    }
+
+    public boolean isBt() {
+        return bt;
+    }
+
+    public void setBt(boolean bt) {
+        this.bt = bt;
+    }
+
+}
---
+++ Cine/src/java/dao/ClienteImpl.java
@@ -0,0 +1,119 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Cliente;
+
+/**
+ *
+ * @author Ctorres
+ */
+public class ClienteImpl extends Conexion implements ICRUD<Cliente> {
+
+    @Override
+    public void regitrar(Cliente cli) throws Exception {
+        this.conectar();
+        try {
+            String sql = "insert into cliente (NOMCLI,APECLI,NACCLI,SEXCLI,ESTCLI) values (?,?,?,?,?)";
+            PreparedStatement ps = getCn().prepareStatement(sql);
+            ps.setString(1, cli.getNOMCLI());
+            ps.setString(2, cli.getAPECLI());
+            ps.setDate(3, new java.sql.Date(cli.getNACCLI().getTime()));
+            ps.setString(4, cli.getSEXCLI());
+            ps.setString(5, "A");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    public List<Cliente> listarcli(String estado) throws Exception {
+        Cliente cliente;
+        List<Cliente> list;
+        this.conectar();
+        try {
+            list = new ArrayList();
+            String sql = "select*from CLIENTE where ESTCLI='" + estado + "'";
+            Statement st = getCn().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                cliente = new Cliente();
+                cliente.setIDCLI(rs.getInt("IDCLI"));
+                cliente.setNOMCLI(rs.getString("NOMCLI"));
+                cliente.setAPECLI(rs.getString("APECLI"));
+                cliente.setNACCLI(rs.getDate("NACCLI"));
+                cliente.setSEXCLI(rs.getString("SEXCLI"));
+                cliente.setESTCLI(rs.getString("ESTCLI"));
+                list.add(cliente);
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            System.out.println("Erroe: " + e);
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+        return list;
+    }
+
+    @Override
+    public void modificar(Cliente cli) throws Exception {
+        this.conectar();
+        try {
+            String sql = "update CLIENTE SET NOMCLI=?, APECLI=?, NACCLI=?, SEXCLI=? WHERE IDCLI=?";
+            PreparedStatement ps = getCn().prepareStatement(sql);
+            ps.setString(1, cli.getNOMCLI());
+            ps.setString(2, cli.getAPECLI());
+            ps.setDate(3, new java.sql.Date(cli.getNACCLI().getTime()));
+            ps.setString(4, cli.getSEXCLI());
+            ps.setInt(5, cli.getIDCLI());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Error dao: " + e);
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+    public void habilitar(Cliente cli) throws Exception{
+         this.conectar();
+        try {
+            String sql = "update CLIENTE  set ESTCLI='A' where IDCLI=?";
+            PreparedStatement ps = getCn().prepareStatement(sql);
+            ps.setInt(1, cli.getIDCLI());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    @Override
+    public void eliminar(Cliente cli) throws Exception {
+        this.conectar();
+        try {
+            String sql = "update CLIENTE  set ESTCLI='I' where IDCLI=?";
+            PreparedStatement ps = getCn().prepareStatement(sql);
+            ps.setInt(1, cli.getIDCLI());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            this.Cerrar();
+        }
+    }
+
+    @Override
+    public List<Cliente> listar() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
